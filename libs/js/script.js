d34225d0c3fe0302c23a74abb3d355ffe3de4f9e
Zack Brady <zackbrady123@gmail.com>
2022-03-15 03:02:13 -0400

    updated/added tests

---
+++ libs/js/script.js
@@ -0,0 +1,33 @@
+var getJSON = function(url, callback) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+
+        var status = xhr.status;
+
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+
+    xhr.send();
+};
+
+getJSON('http://time.jsontest.com',  function(err, data) {
+
+    if (err != null) {
+        console.error(err);
+    } else {
+
+        var text = `Date: ${data.date}
+                    Time: ${data.time}
+                    Unix time: ${data.milliseconds_since_epoch}`
+
+        console.log(text);
+    }
+});
